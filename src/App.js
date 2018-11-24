da63d8eeb72bd73e69da338ba186d93b22a0f124
Kevin Boyle <papershack@gmail.com>
2018-11-23 23:21:02 -0800

    refactor to keep state & actions in context.js instead of locally in component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Provider } from './context';
 import Header from './components/Header';
 import Contacts from './components/Contacts';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <h1>App Component</h1>
-          <Header />
-          <div className="container">
-            <Contacts />
-          </div>
-      </div>
+      <Provider>
+        <div className="App">
+            <h1>App Component</h1>
+            <Header />
+            <div className="container">
+              <Contacts />
+            </div>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,56 +1,29 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { Consumer } from '../context';
 
 class Contacts extends Component {
-    state = {
-        contacts: [
-            {
-                id: 1,
-                name: 'John Doe',
-                email: 'jdoe@gmail.com',
-                phone: '555-555-5555'
-            },
-            {
-                id: 2,
-                name: 'Jen Blue',
-                email: 'jblue@gmail.com',
-                phone: '555-556-6655'
-            },
-            {
-                id: 3,
-                name: 'Harry Black',
-                email: 'hblack@gmail.com',
-                phone: '555-557-6755'
-            }
-        ]
-    };
-
-    deleteContact = id => {
-        console.log(id);
-        const { contacts } = this.state;
-//filter our contacts array as to where the current contact.id is not included
-        const newContacts = contacts.filter(contact =>
-            contact.id !== id);
-
-            this.setState({
-                contacts: newContacts
-        });
-    };
 
     render(){
-        const { contacts } = this.state;
-        return (
-            <div>
-                {contacts.map(contact => (
-                    <Contact 
-                        key={contact.id}
-                        contact={contact}
-                        deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-                    />
-                ))}
-            </div>
-        )
-    }
+    return (
+        <Consumer>
+            {value => {
+                const { contacts } = value;
+                return (
+                    <React.Fragment>
+                        {contacts.map(contact => (
+                            <Contact 
+                                key={contact.id}
+                                contact={contact}
+                            />
+                        ))}
+                    </React.Fragment>
+                );
+            }}
+        </Consumer>
+    );
+};
 }
+
 
 export default Contacts;
