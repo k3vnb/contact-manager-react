c747b0b680d9fb7db0829ba755e5b01f0665ee20
Kevin Boyle <papershack@gmail.com>
2018-11-24 18:58:25 -0800

    add validation to form in AddContacts

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,13 +7,26 @@ class AddContact extends Component {
     state = {
         name: '',
         email: '',
-        phone: ''
+        phone: '',
+        errors: {}
     }
 
     onSubmit = (dispatch, e) => {
         e.preventDefault();
         const { name, email, phone } = this.state;
-
+// Check for Errors
+        if(name === ''){
+            this.setState({errors: {name: 'Name is required' }});
+            return;
+        }
+        if(email === ''){
+            this.setState({errors: {email: 'Email is required' }});
+            return;
+        }
+        if(phone === ''){
+            this.setState({errors: {phone: 'Phone is required' }});
+            return;
+        }
         const newContact = {
             id: uuid(),
             name,
@@ -26,13 +39,14 @@ class AddContact extends Component {
         this.setState({
             name: '',
             email: '',
-            phone: ''
+            phone: '',
+            errors: {}
         })
     };
 
     onChange = e => this.setState({ [e.target.name]: e.target.value })
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
         <Consumer>
@@ -49,6 +63,7 @@ class AddContact extends Component {
                             placeholder="Enter Name..."
                             value={name}
                             onChange={this.onChange}
+                            error={errors.name}
                         />
                         <TextInputGroup
                             label="Email"
@@ -56,6 +71,7 @@ class AddContact extends Component {
                             placeholder="Enter Email..."
                             value={email}
                             onChange={this.onChange}
+                            error={errors.email}
                         />
                         <TextInputGroup
                             label="Phone"
@@ -63,6 +79,7 @@ class AddContact extends Component {
                             placeholder="Enter Phone..."
                             value={phone}
                             onChange={this.onChange}
+                            error={errors.phone}                    
                         />
 
                         <input 
--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 
 const TextInputGroup = ({
@@ -8,7 +9,8 @@ const TextInputGroup = ({
     value,
     placeholder,
     type,
-    onChange
+    onChange,
+    error
 }) => {
   return (
         <div className="form-group">
@@ -16,11 +18,16 @@ const TextInputGroup = ({
             <input 
                 type={type} 
                 name={name}
-                className="form-control form-control-lg"
+                className={classnames('form-control form-control-lg', {
+                    'is-invalid': error
+                })}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
+
             />
+            {error && <div className="invalid-feeback">{error}</div>}
+            
         </div>
   );
 };
@@ -31,7 +38,8 @@ TextInputGroup.propTypes = {
     value: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    error: PropTypes.string
 }
 
 TextInputGroup.defaultProps = {
