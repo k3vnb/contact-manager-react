37715fc216165109c090016280faf5744eb51a23
Kevin Boyle <papershack@gmail.com>
2018-11-25 17:39:08 -0800

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Contact Manager
-
-## This app is based on Brad Traversy's course "React Front to Back" on Udemy.com. 
-
-It is a CRUD app, utilizing create-react-app, bootstrap, the 'context' API instead of Redux, with Axios calls to https://jsonplaceholder.typicode.com/ for mock data.
-
-### Users can add, edit, and delete contacts. The changes are not persisted to a database, but they are made to the DOM.
---
+++ precache-manifest.e1c25a4ecf472bb45db1889619a8d411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee18c3c0f4ec063fbec3",
+    "url": "/contact-manager-react/static/js/runtime~main.ee18c3c0.js"
+  },
+  {
+    "revision": "201a293633bd6934d846",
+    "url": "/contact-manager-react/static/js/main.201a2936.chunk.js"
+  },
+  {
+    "revision": "3f16e2adae0a7a863bf0",
+    "url": "/contact-manager-react/static/js/1.3f16e2ad.chunk.js"
+  },
+  {
+    "revision": "201a293633bd6934d846",
+    "url": "/contact-manager-react/static/css/main.dca13416.chunk.css"
+  },
+  {
+    "revision": "3f16e2adae0a7a863bf0",
+    "url": "/contact-manager-react/static/css/1.e6b6446d.chunk.css"
+  },
+  {
+    "revision": "ba348ab1d9774108cc9a3b649f5aab6f",
+    "url": "/contact-manager-react/index.html"
+  }
+];
